d935820a8ac079adeb3e75aacf5d350a0dbb28f2
Anthony <github email address>
2018-09-10 22:53:06 -0400

    added user validations to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_one :cart
   has_secure_password
 
+  before_validation :downcase_email
+  validates_presence_of :email, :password, :first_name, :last_name, :address, :city, :state_initials, :zip
+  validates_uniqueness_of :email
+  validates :state_initials, length: { maximum: 2 }
+  validates :zip, length: { is: 5 }
 
 
 #users are initialized with a cart
