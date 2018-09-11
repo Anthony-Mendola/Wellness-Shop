2d5bbf5d2204ecabcc8f8e2d8ffaa1e604b77036
Anthony <github email address>
2018-09-10 21:58:13 -0400

    created models

---
+++ db/migrate/20180911015105_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :img_full
+      t.string :img_thumbnail
+      t.text :description
+      t.integer :price
+      t.boolean :sold_out, default: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20180911015253_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :state_initials
+      t.integer :zip
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
