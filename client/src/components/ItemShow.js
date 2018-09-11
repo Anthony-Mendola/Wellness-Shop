de07bf8a14dfe0ef458548da97726e18de075e3b
Anthony <github email address>
2018-09-11 15:23:30 -0400

    missing brackert in itemshow component

--- client/src/components/ItemShow.js
+++ client/src/components/ItemShow.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
-import AddToCart from '../components/AddToCart'
+import React, { Component } from "react";
+import AddToCart from "../components/AddToCart";
 
 class ItemShow extends Component {
-
   componentDidMount() {
-    this.props.actions.fetchItem(this.props)
+    this.props.actions.fetchItem(this.props);
   }
 
   render() {
-    const item = this.props.item
-
+    const item = this.props.item;
 
     return (
-      <div className='item-div'>
-        <a href='/items'>Back</a>
+      <div className="item-div">
+        <a href="/items">Back</a>
         <h2>{item.name}</h2>
         <img src={item.img_full} alt={item.name} />
-        <h3><em>${item.price}</em></h3>
-        { this.props.cartId && <AddToCart cartId={this.props.cartId} itemId={this.props.itemId}/>}
-        <p dangerouslySetInnerHTML={{__html: item.description}} />
+        <h3>
+          <em>${item.price}</em>
+        </h3>
+        {this.props.cartId && (
+          <AddToCart cartId={this.props.cartId} itemId={this.props.itemId} />
+        )}
+        <p dangerouslySetInnerHTML={{ __html: item.description }} />
       </div>
-    )
+    );
   }
+}
+
+export default ItemShow;
