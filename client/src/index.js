b07391c3292b08ced98aa0fd310baf2fae36b635
Anthony <github email address>
2018-09-11 14:28:00 -0400

    added store and root reducer to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
 import "./index.css";
 import App from "./App";
 import "semantic-ui-css/semantic.css";
 import rootReducer from "./reducers/rootReducer";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
