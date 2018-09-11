4758635fe2a3bb48c3ec98dcd01c757a4eba4a1a
Anthony <github email address>
2018-09-11 17:18:25 -0400

    created a checkout cart component and updated cart container

---
+++ client/src/components/CheckoutCart.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../actions/userActions";
+import { Form, Button } from "semantic-ui-react";
+
+class CheckoutCart extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.actions.checkoutCart(this.props);
+  };
+
+  render() {
+    return (
+      <div>
+        <Form id="checkout-cart" onSubmit={this.handleSubmit}>
+          <input name="cart_id" type="hidden" value={this.props.cartId} />
+          <Button type="submit" fluid inverted disabled={this.props.disabled}>
+            Confirm Order
+          </Button>
+        </Form>
+        <br />
+        {this.props.itemtIds}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  if (state.user.cart.items.length > 0) {
+    return {
+      cartId: state.user.cart.id,
+      disabled: false
+    };
+  } else {
+    return {
+      cartId: state.user.cart.id,
+      disabled: true
+    };
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actions, dispatch) };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CheckoutCart);
---
+++ client/src/containers/Cart.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../actions/itemActions";
+
+import { Header, Container, Divider } from "semantic-ui-react";
+import ItemsList from "../components/ItemsList";
+import CheckoutCart from "../components/CheckoutCart";
+
+class Cart extends Component {
+  render() {
+    const user = this.props.user;
+    return (
+      <div>
+        <Container>
+          <Header as="h2" textAlign="center">
+            Your Cart
+            <Divider hidden />
+          </Header>
+          <div className="page-container">
+            <Divider />
+            <div className="cart-left">
+              <ItemsList page={"cart"} items={user.cart.items} />
+            </div>
+            <div className="cart-right">
+              <h3>
+                Total: <em>${this.props.total}</em>
+              </h3>
+              <Divider className="cart-divider" />
+              <h3>Address:</h3>
+              {user.first_name} {user.last_name}
+              <br />
+              {user.address}
+              <br />
+              {user.city}, {user.state_initials} {user.zip}
+              <br />
+              <Divider hidden />
+              {}
+              <CheckoutCart />
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+    total: state.user.cart.products
+      .map(product => product.price)
+      .reduce((a, b) => a + b, 0)
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actions, dispatch) };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Cart);
