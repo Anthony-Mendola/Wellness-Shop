692a2ac5521f35a0fc2d43788613d6bdf786870f
Anthony <github email address>
2018-09-12 15:58:58 -0400

    working on order confirmation page

--- client/src/components/OrderConfirmation.js
+++ client/src/components/OrderConfirmation.js
@@ -3,10 +3,12 @@ import { Header, Container, Divider } from "semantic-ui-react";
 
 const OrderConfirmation = () => (
   <Container>
-    <Header as="h2" textAlign="center">
+    <Header color="teal" as="h1" textAlign="center">
       Thank you for your order!
       <Divider hidden />
       <h3>Your item(s) will be shipped within 1 business days.</h3>
+      <h3>If you're interested in hiring a health coach, click the link below!</h3>
+      <a href="http://www.wellnesswithanthony.com/" target="_blank">Click Here</a>
     </Header>
   </Container>
 );
