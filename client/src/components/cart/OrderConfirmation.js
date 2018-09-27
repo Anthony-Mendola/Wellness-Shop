f732cf25da1111091c25ae20e719f59c6ab1e565
Anthony <github email address>
2018-09-27 15:02:52 -0400

    still troubleshooting

--- client/src/components/cart/OrderConfirmation.js
+++ client/src/components/cart/OrderConfirmation.js
@@ -8,7 +8,7 @@ const OrderConfirmation = () => (
       <Divider hidden />
       <h3>Your item(s) will be shipped within 1 business days.</h3>
       <h3>If you're interested in hiring a health coach, click the link below!</h3>
-      <a href="http://www.wellnesswithanthony.com/" target="_blank">Click Here</a>
+      <a href="https://www.wellnesswithanthony.com/" target="_blank">Click Here</a>
     </Header>
   </Container>
 );
